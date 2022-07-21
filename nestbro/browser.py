85769204919cb6d0f45df4ef002f975d093e98f5
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-22 08:12:28 +0900

    Update browser.py

--- nestbro/browser.py
+++ nestbro/browser.py
@@ -5,5 +5,32 @@ import subprocess
 
 
 class Browser:
-    def __init__(self, headless: bool = True):
+    DIVICE_URI: str | None = None
+    def __init__(self, headless: bool = True, *, executor_path: str = "chromium-browser",
+                 gpu: bool = False, sandbox: bool = False):
         self.headless = headless
+        self.process: subprocess.Popen | None = None
+        self.executor_path = executor_path
+        self.gpu = gpu
+        self.sandbox = sandbox
+        self.client = AsyncClient()
+
+    async def start(self, port: int = 9222):
+        args = [self.executor_path]
+        if self.headless:
+            args.append("--headless")
+        if not self.gpu:
+            args.append("--disable-gpu")
+        if not self.sandbox:
+            args.append("--no-sandbox")
+        args.append(f"--remote-debugging-port={port}")
+        self.process = subprocess.Popen(args)
+        self.URI = "http://127.0.0.1:{}".format(port)
+        self.ws = await connect(await self.get_ws_url())
+
+    async def request(self, method: str, path: str, **kwargs) -> dict:
+        r = await self.client.request(method, self.URI + path, **kwargs)
+        return r.json()
+ 
+    async def get_ws_url(self) -> str:
+        return await self.request("GET", "/json")[0]["webSocketDebuggerUrl"]
