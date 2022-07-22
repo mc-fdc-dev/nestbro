6bd4512799799c3d3f7db22bf359fe9d4b3b4db7
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-22 09:38:34 +0900

    Update pages.py

--- nestbro/pages.py
+++ nestbro/pages.py
@@ -2,6 +2,7 @@ from websockets import connect
 from orjson import dumps
 
 from typing import Any
+from base64 import b64decode
 
 
 class Page:
@@ -13,3 +14,19 @@ class Page:
 
     async def request(self, method: str, params: Any, *, type: int = 1) -> None:
         await self.ws.send(dumps({"type": type, "method": method, "params": params}))
+        
+    async def recv(self) -> dict:
+        return dumps(await self.ws.recv())
+
+    async def close(self, *, ignore_cache: bool = False,
+                    script_to_evaluate_on_load: str | None = None) -> None:
+        payload = {}
+        if ignore_cache:
+            payload["ignoreCache"] = True
+        if script_to_evaluate_on_load is not None:
+            payload["scriptToEvaluateOnLoad"] = script_to_evaluate_on_load
+        await self.request(method="Page.reload", params=payload)
+        
+    async def screenshot(self) -> bytes:
+        await self.request("Page.captureScreenshot")
+        return b64decode((await self.recv()["data"]).encode())
